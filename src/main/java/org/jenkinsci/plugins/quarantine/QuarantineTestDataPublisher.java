438a77c5b460699e352ad983f168ff9104a89d9e
Dan Mincu <dan.mincu@jsitelecom.com>
2018-11-26 09:29:42 -0500

    cleaned a commented method.

--- src/main/java/org/jenkinsci/plugins/quarantine/QuarantineTestDataPublisher.java
+++ src/main/java/org/jenkinsci/plugins/quarantine/QuarantineTestDataPublisher.java
@@ -92,65 +92,6 @@ public class QuarantineTestDataPublisher extends TestDataPublisher {
 
    }
 
-   /*
-   @Override
-   public Data getTestData(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener, TestResult testResult) {
-
-      Data data = new Data(build);
-      MailNotifier notifier = new MailNotifier(listener);
-
-      for (SuiteResult suite : testResult.getSuites()) {
-         for (CaseResult result : suite.getCases()) {
-            QuarantineTestAction previousAction = null;
-            CaseResult previous = result.getPreviousResult();
-            AbstractBuild<?, ?> previousBuild = build.getPreviousCompletedBuild();
-
-            if (previous != null) {
-               previousAction = previous.getTestAction(QuarantineTestAction.class);
-            }
-
-            // no immediate predecessor (e.g. because job failed or did not
-            // run), try and go back in build history
-            while (previous == null && previousBuild != null) {
-               if (previousBuild.getAction(AbstractTestResultAction.class) != null) {
-                  hudson.tasks.test.TestResult tr = null;
-                  try {
-                     tr = previousBuild.getAction(AbstractTestResultAction.class).findCorrespondingResult(
-                             result.getId());
-                  }
-                  catch (Exception e){
-                     listener.getLogger().println("could not find result for id " + result.getId() + " in build " + previousBuild + ": " + e.getMessage());
-                  }
-                  if (tr != null) {
-                     listener.getLogger().println("found " + tr.getDisplayName() + " in build " + previousBuild);
-                     previousAction = tr.getTestAction(QuarantineTestAction.class);
-                     break;
-                  }
-               }
-               else
-               {
-                  listener.getLogger().println("build " + previousBuild + " does not have test results");
-               }
-               previousBuild = previousBuild.getPreviousCompletedBuild();
-            }
-
-            if (previousAction != null && previousAction.isQuarantined()) {
-               QuarantineTestAction action = new QuarantineTestAction(data, result.getId());
-               action.quarantine(previousAction);
-               data.addQuarantine(result.getId(), action);
-
-               // send email if failed
-               if (!result.isPassed()) {
-                  notifier.addResult(result, action);
-               }
-            }
-         }
-      }
-      notifier.sendEmails();
-      return data;
-   }
-   */
-
    public static class Data extends TestResultAction.Data implements Saveable {
 
       private Map<String, QuarantineTestAction> quarantines = new HashMap<>();
